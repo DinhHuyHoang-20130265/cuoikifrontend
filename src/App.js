a105fa33df1a54471e6b4f5807ad6130b17e9f47
DinhHuyHoang-20130265 <20130265@st.hcmuaf.eduvn>
2023-05-04 18:04:51 +0700

    create project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import 'mdbreact/dist/css/mdb.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
