95915d5b21792f9ef199840a8da948ae4b615771
DinhHuyHoang-20130265 <20130265@st.hcmuaf.edu.vn>
2023-06-06 23:38:28 +0700

    nothing

--- src/components/ContentOfPost.js
+++ src/components/ContentOfPost.js
@@ -40,7 +40,6 @@ function ContentOfPost(props) {
         <Link to={`/${props.cate}`} className="f1-s-10 cl2 hov-cl10 trans-03 text-uppercase">
             {catedatas.find(item => item.cate === props.cate).name}
         </Link>
-
         <h3 className="f1-l-3 cl2 p-b-20 p-t-33 respon2">
             {content.title}
         </h3>
@@ -58,7 +57,6 @@ function ContentOfPost(props) {
         <h5 className={"sapo p-b-15"}>{content.sapo}</h5>
 
         <div className={"main-content"} ref={contentRef}/>
-
         <div className="flex-s-s p-t-12 p-b-15">
 								<span className="f1-s-12 cl5 m-r-8">
 									Tags:
