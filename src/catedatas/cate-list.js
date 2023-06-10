fc623c45e9484f1f2d5072dc203413607808e07e
DinhHuyHoang-20130265 <20130265@st.hcmuaf.edu.vn>
2023-06-10 17:53:55 +0700

    fix loi sai chinh ta

--- src/catedatas/cate-list.js
+++ src/catedatas/cate-list.js
@@ -13,7 +13,7 @@ export const catedatas = [{name: "Tin mới nhất", cate: "tin-moi-nhat", id: 1
 }, {name: "Công nghệ", cate: "cong-nghe", id: 12}, {name: "Du lịch xanh", cate: "du-lich-xanh", id: 13}, {
     name: "Lý tưởng sống",
     cate: "ly-tuong-song", id: 14
-}, {name: "Nói thẳng", cate: "noi-thang", id: 15}, {name: "Tin độc quyên", cate: "tin-doc-quyen", id: 16}, {
+}, {name: "Nói thẳng", cate: "noi-thang", id: 15}, {name: "Tin độc quyền", cate: "tin-doc-quyen", id: 16}, {
     name: "Địa ốc",
     cate: "dia-oc", id: 17
 }, {name: "Phụ nữ", cate: "chuyen-trang-phu-nu", id: 18}, {name: "Thị trường", cate: "thi-truong", id: 19},
